2348070fb4bf7c70a3e8ff9c76d2d0585cc98898
flowerinthenight <chew.esmero@gmail.com>
2017-12-11 06:07:12 +0000

    Add missing file.

---
+++ pre.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker --version
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+mkdir ${HOME}/.kube
+cp ./kubeconf.yaml ${HOME}/.kube/config
+kubectl config set clusters.mochi.k8s.local.certificate-authority-data ${KUBE_CLUSTER_CERT}
+kubectl config set clusters.mochi.k8s.local.server ${KUBE_SERVER}
+kubectl config set users.mochi.k8s.local.client-certificate-data ${KUBE_CLIENT_CERT}
+kubectl config set users.mochi.k8s.local.client-key-data ${KUBE_CLIENT_KEYDATA}
+kubectl version
+install --user awscli
+export PATH=${PATH}:${HOME}/.local/bin
+aws --version
