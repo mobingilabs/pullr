6493c49381f401b47975378a69d6023ee045f92d
flowerinthenight <chew.esmero@gmail.com>
2017-12-11 06:27:20 +0000

    Try env vars.

--- pre.sh
+++ pre.sh
@@ -6,10 +6,24 @@ chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 mkdir ${HOME}/.kube
 cp ./kubeconf.yaml ${HOME}/.kube/config
-kubectl config set clusters.mochi.k8s.local.certificate-authority-data ${KUBE_CLUSTER_CERT}
-kubectl config set clusters.mochi.k8s.local.server ${KUBE_SERVER}
-kubectl config set users.mochi.k8s.local.client-certificate-data ${KUBE_CLIENT_CERT}
-kubectl config set users.mochi.k8s.local.client-key-data ${KUBE_CLIENT_KEYDATA}
+
+# 'master' is our development branch
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+    echo "Setting up development environment"
+    kubectl config set clusters.mochi.k8s.local.certificate-authority-data ${KUBE_CLUSTER_CERT}
+    kubectl config set clusters.mochi.k8s.local.server ${KUBE_SERVER}
+    kubectl config set users.mochi.k8s.local.client-certificate-data ${KUBE_CLIENT_CERT}
+    kubectl config set users.mochi.k8s.local.client-key-data ${KUBE_CLIENT_KEYDATA}
+fi
+
+if [[ "$TRAVIS_BRANCH" == "production" ]] || [[ "$TRAVIS_BRANCH" == "$TRAVIS_TAG" ]]; then
+    echo "Setting up production environment"
+    kubectl config set clusters.mochi.k8s.local.certificate-authority-data ${KUBE_PROD_CLUSTER_CERT}
+    kubectl config set clusters.mochi.k8s.local.server ${KUBE_PROD_SERVER}
+    kubectl config set users.mochi.k8s.local.client-certificate-data ${KUBE_PROD_CLIENT_CERT}
+    kubectl config set users.mochi.k8s.local.client-key-data ${KUBE_PROD_CLIENT_KEYDATA}
+fi
+
 kubectl version
 pip install --user awscli
 export PATH=${PATH}:${HOME}/.local/bin
