71392bb0e920a9ab7fb3ea0ede4e8e09cbb6c9f7
flowerinthenight <chew.esmero@gmail.com>
2017-12-12 04:29:27 +0000

    Limit deploy.

--- cmd/apiserver/deploy.sh
+++ cmd/apiserver/deploy.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-# login to ecr
-export AWS_ACCESS_KEY_ID=`echo ${PULLRCI_ACCESS_KEY_ID}`
-export AWS_SECRET_ACCESS_KEY=`echo ${PULLRCI_SECRET_ACCESS_KEY}`
-`aws ecr get-login --region ap-northeast-1`
+DEPLOY=0
+
+if [[ "$TRAVIS_BRANCH" == "master" ]]i || [[ "$TRAVIS_BRANCH" == "production" ]]; then
+    DEPLOY=1
+fi
 
 IMAGE_TAG=`echo ${TRAVIS_COMMIT}`
 
 if [[ "$TRAVIS_BRANCH" == "$TRAVIS_TAG" ]]; then
     IMAGE_TAG=${TRAVIS_TAG}
+    DEPLOY=1
 fi
+
+if [[ $DEPLOY -eq 0 ]]; then
+    echo "Deploy not supported"
+    exit 0
+fi
+
+# login to ecr
+export AWS_ACCESS_KEY_ID=`echo ${PULLRCI_ACCESS_KEY_ID}`
+export AWS_SECRET_ACCESS_KEY=`echo ${PULLRCI_SECRET_ACCESS_KEY}`
+`aws ecr get-login --region ap-northeast-1`
 
 # push image to ecr
 IMAGE="${NAME}:${IMAGE_TAG}"
