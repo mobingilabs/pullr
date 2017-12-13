ee1940664753afd345f534b77b606f764d9a3513
flowerinthenight <chew.esmero@gmail.com>
2017-12-13 03:15:03 +0000

    Fix docker login deploy.

--- cmd/apiserver/deploy.sh
+++ cmd/apiserver/deploy.sh
@@ -21,7 +21,7 @@ fi
 # login to ecr
 export AWS_ACCESS_KEY_ID=`echo ${PULLRCI_ACCESS_KEY_ID}`
 export AWS_SECRET_ACCESS_KEY=`echo ${PULLRCI_SECRET_ACCESS_KEY}`
-`aws ecr get-login --region --no-include-email ap-northeast-1`
+`aws ecr get-login --no-include-email --region ap-northeast-1`
 
 # push image to ecr
 IMAGE="${NAME}:${IMAGE_TAG}"
