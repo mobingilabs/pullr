db86193aac6fb1bf082cafc0620ce34ab46dbdc6
flowerinthenight <chew.esmero@gmail.com>
2017-12-13 03:23:18 +0000

    Check err deploy.

--- cmd/apiserver/deploy.sh
+++ cmd/apiserver/deploy.sh
@@ -23,12 +23,20 @@ export AWS_ACCESS_KEY_ID=`echo ${PULLRCI_ACCESS_KEY_ID}`
 export AWS_SECRET_ACCESS_KEY=`echo ${PULLRCI_SECRET_ACCESS_KEY}`
 `aws ecr get-login --no-include-email --region ap-northeast-1`
 
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 # push image to ecr
 IMAGE="${NAME}:${IMAGE_TAG}"
 echo "image = ${IMAGE}"
 docker tag ${IMAGE} ${ECR_REPO_URI}/${IMAGE}
 docker images
 docker push ${ECR_REPO_URI}/${IMAGE}
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 
 # update kubernetes deployment with the new image
 kubectl set image deployment ${NAME} ${NAME}=${ECR_REPO_URI}/${IMAGE}
